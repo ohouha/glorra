7318cdfa17d068e69629f1dbfe2d9003707298d3
ohouha <wanyanzheng@gmail.com>
2016-07-06 21:11:44 -0700

    second controller one player finish

---
+++ prototype/prototype/Assets/script/player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    public joystick joysticker;
+	public Transform target;
+    private Vector3 addOn;
+	private float leftBorder, rightBorder, topBorder, bottomBorder;
+
+	void Start () {
+		float dist=(transform.position - Camera.main.transform.position).z;
+		leftBorder=Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+		rightBorder=Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+		topBorder=Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+		bottomBorder=Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+		Debug.Log (topBorder);
+		Debug.Log (bottomBorder);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//addOn = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0f);  //&&transform.position.x<bottom&&transform.position.y<left&&transform.position.y<right
+		addOn = new Vector3(joysticker.Horizontal(), joysticker.Vertical(), 0f);
+		transform.position+=addOn * speed * Time.deltaTime;
+		float x = Mathf.Clamp (transform.position.x, leftBorder, rightBorder);
+		float y=Mathf.Clamp(transform.position.y,bottomBorder,topBorder);
+		/*if (transformPosition.y < top && transformPosition.y > bottom && transformPosition.x > left && transformPosition.x < right) {
+			transform.position = transformPosition;
+		}*/ 
+		transform.position = new Vector3 (x,y,0);
+
+		Vector3 pos = target.transform.position - transform.position;
+		float angle = Mathf.Atan2 (pos.y, pos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+
+
+    }
+
+}
---
+++ prototype/prototype/Assets/script2/joystick2.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class joystick2 : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    private Image bgImg;
+    private Image joyStick;
+    private Vector2 inputVector;
+    public int playerNumber;
+    public bool draggable;
+    // Use this for initialization
+    void Start()
+    {
+        bgImg = GetComponent<Image>();
+        joyStick = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform,
+            eventData.position,
+            eventData.pressEventCamera, out pos))
+        {
+            //get 0-1 value
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+            if (playerNumber == 1)
+            {
+                inputVector = new Vector2(pos.x * 2 - 1, pos.y * 2 - 1);
+            }
+            if (playerNumber == 2)
+            {
+                inputVector = new Vector2(pos.x * 2 + 1, pos.y * 2 - 1);
+            }
+
+            Debug.Log(inputVector);
+
+            inputVector = (inputVector.magnitude > 1.0) ? inputVector.normalized : inputVector;
+
+
+
+            //move joystickImage
+            joyStick.rectTransform.anchoredPosition =
+                new Vector2(inputVector.x * (bgImg.rectTransform.sizeDelta.x / 3),
+                inputVector.y * (bgImg.rectTransform.sizeDelta.y / 3));
+
+            //Debug.Log(inputVector.magnitude);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector2.zero;
+        joyStick.rectTransform.anchoredPosition = Vector2.zero;
+        draggable = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        draggable = true;
+        OnDrag(eventData);
+    }
+
+    public float Rotation()
+    {
+        if (inputVector.x != 0 || inputVector.y != 0)
+        {
+            return Mathf.Atan2(inputVector.y, inputVector.x) * Mathf.Rad2Deg;
+
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+
+}
